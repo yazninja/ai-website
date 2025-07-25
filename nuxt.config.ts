d49a309cab80cb31ad21ff0f6c52c76e5f7a6bfe
Yazninja <yazlesean@gmail.com>
2025-07-25 17:43:42 +0300

    use cloudflare pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   nitro: {
-    preset: "cloudflare_module",
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true
+    prerender: {
+      autoSubfolderIndex: false
     }
   },
   modules: [
