f52b2f1386409a527842f71191e8451d258cf4ce
yazninja <yazlesean@gmail.com>
2026-01-22 01:25:15 +0300

    dynamic hostname

--- app/components/TwichEmbed.vue
+++ app/components/TwichEmbed.vue
@@ -1,8 +1,13 @@
 <template>
 	<div class="twitch-embed-wrapper w-full max-w-3xl mx-auto my-8 px-4">
-		<div class="twitch-embed-container" style="aspect-ratio:16/9;max-width:none;min-width:100%!important;"><iframe
-				src="https://player.twitch.tv/?channel=uchuujin_ai&parent=uchuujinai.com&autoplay=false&muted=false&time=0s"
+		<div class="twitch-embed-container" style="aspect-ratio:16/9;max-width:none;min-width:100%!important;">
+			<iframe
+				:src="'https://player.twitch.tv/?channel=uchuujin_ai&parent=' + domain + '&autoplay=false&muted=false&time=0s'"
 				width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
 		</div>
 	</div>
 </template>
+<script setup lang="ts">
+const $route = useRequestURL();
+const domain = $route.hostname;
+</script>
