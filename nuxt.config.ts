f87d25fd1b9da19a0635c658249b88761e162afe
yazninja <yazlesean@gmail.com>
2026-01-23 16:57:55 +0300

    add d1 config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2026-01-23',
   devtools: { enabled: true },
   nitro: {
+    cloudflare: {
+      deployConfig: true,
+      wrangler: {
+        d1_databases: [
+          {
+            binding: 'DB',
+            database_name: 'd1-sql',
+            database_id: 'f5c31d83-f039-4d50-904f-7cab4920024f'
+          }
+        ]
+      }
+    },
     prerender: {
       autoSubfolderIndex: false
     }
